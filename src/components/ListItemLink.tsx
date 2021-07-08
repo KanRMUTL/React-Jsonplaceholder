47b70b4f0465c9363c7b985b21eb7298237dbfbf
Kiadtisak Lowongsa <ktstomember@gmail.com>
2021-07-08 15:26:15 +0700

    hilighted selected menu

--- src/components/ListItemLink.tsx
+++ src/components/ListItemLink.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { List, ListItem, ListItemText } from '@material-ui/core';
 import { Route } from '../route/path';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 interface Props {
   routes: Route[];
   onSelectedRoute?: () => void;
 }
 export default function ListItemLink({ routes, onSelectedRoute }: Props) {
+  let { pathname } = useLocation();
   return (
     <List component="nav">
       {routes.map(({ path, label }, index) => (
-        <ListItem button key={index} component={Link} to={path} onClick={onSelectedRoute}>
+        <ListItem button key={index} component={Link} to={path} onClick={onSelectedRoute} selected={path === pathname}>
           <ListItemText primary={label} />
         </ListItem>
       ))}
